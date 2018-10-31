f5af7b9e5b5e9f897be0d44753fa8e6f5bed38ca
Elliot Liebman <elgincheese@gmail.com>
2018-10-31 13:30:52 -0500

    Initial Commit

---
+++ src/Components/Photos.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Photo from './Photo';
+
+const Photos = ({ photos, category }) => {
+  return (
+    <div className="photo-container">
+      <h2>results</h2>
+      <ul>
+        {
+          photos.map( photo => <Photo url={ photo.url } key={ photo.key } /> )
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default Photos;
